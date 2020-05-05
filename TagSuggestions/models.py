dcc2425ebde524055c72a8be80ee11210b0c3a1a
Matej2 <11059438+matej2@users.noreply.github.com>
2020-05-05 22:09:26 +0200

    Random hashtags

--- TagSuggestions/models.py
+++ TagSuggestions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class HashtagSearch():
+    def __init__(self, word, first):
+        self.word = word
+        # "First" parameter makes sure only most popular hashtags are included, assuming list is properly ordered
+        self.first = first
--- TagSuggestions/views.py
+++ TagSuggestions/views.py
@@ -1,31 +1,38 @@
 import json
+from random import shuffle
 
 from django.http import JsonResponse
 from django.shortcuts import render
 import requests
 import re
 
+from TagSuggestions.models import HashtagSearch
+
+tag_search = []
+tag_search.append(HashtagSearch("handmade", 50))
+tag_search.append(HashtagSearch("homemade", 30))
+tag_search.append(HashtagSearch("slovenia", 5))
+
+hashtag_list = []
 
 def random_hashtags(request):
-    URL = 'https://www.instagram.com/explore/tags/handmade/?__a=1'
 
-    # sending get request and saving the response as response object
-    r = requests.get(url=URL)
-    data = (r.json())
+    for search in tag_search:
+        URL = f'https://www.instagram.com/web/search/topsearch/?context=blended&query=%23{search.word}&rank_token=0.08962517317904317&include_reel=true'
 
-    posts = data["graphql"]["hashtag"]["edge_hashtag_to_top_posts"]["edges"]
+        r = requests.get(url=URL)
+        data = (r.json())
 
-    all_hashtags = []
+        hashtags = data["hashtags"]
 
-    for post in posts:
-        content = post["node"]
-        caption = content["edge_media_to_caption"]["edges"]
+        for i in range(50):
+            if i >= search.first:
+                break
+            text = hashtags[i]["hashtag"]["name"]
+            hashtag_list.append(text)
+
+    shuffle(hashtag_list)
+
     return JsonResponse((
-        type(caption)
+        hashtag_list[:30]
     ), safe=False)
-        #hashtags = re.search(r'(#\w*)\s', caption)
-        #for i in range(5):
-       #     all_hashtags.append(hashtags[i])
-
-    #return JsonResponse(all_hashtags, safe=False)
-
